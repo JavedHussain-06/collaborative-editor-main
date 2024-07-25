9e6795225763c5bd44894ca245dae3a6db8e724f
Javed <50339361+JavedHussain-06@users.noreply.github.com>
2024-07-25 08:14:47 +0530

    LiveDossier

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://dfbb009a6eb9658c8c5958c414c72d3a@o4507659892621312.ingest.us.sentry.io/4507659905925120",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
